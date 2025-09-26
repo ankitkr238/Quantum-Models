6ac45b818f74dc43c6f2fdd0fa9c7f00be463097
ankitkr238 <127776926+ankitkr238@users.noreply.github.com>
2025-09-27 02:08:25 +0530

    Add files via upload

---
+++ Finite diff.py
@@ -0,0 +1,51 @@
+# Finite difference method
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as sc
+
+m=9.1*(10**-31)
+a=-50e-10
+b=50e-10
+n=1000
+d=(b-a)/(n)
+
+x=np.linspace(a,b,n)
+c=2*m*d**2/sc.hbar**2
+
+#Potential
+def V(x):
+##    k=100
+##    return 0.5*k*x**2
+    return 0
+##    if np.abs(x)<5:
+##        return 1
+##    else:
+##        return 0
+
+Vs=[]
+for i in range(len(x)):
+    Vs.insert(i,V(x[i]))
+
+H=np.empty((n,n))
+for i in range(0,n):
+    H[i][i]=2+c*Vs[i]
+    if i>0:
+        H[i][i-1]=-1
+    if i<(n-1):
+        H[i][i+1]=-1
+
+Eg_val,Eg_vec=np.linalg.eigh(H)
+
+print("Energy Eigen Values:")
+for i in range(3):
+    Energy=Eg_val[i]*sc.hbar**2/2/m/d**2/sc.e
+    print(Energy,'eV')
+    plt.plot(x,Eg_vec.T[i],label=(str(Energy)+"eV"))
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("Wave-function")
+plt.axvline(x=a)
+plt.axvline(x=b)
+plt.title("Particle in a Box")
+plt.show()
